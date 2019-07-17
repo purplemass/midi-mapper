40afb69265d4256e077b62d2d3eee3c214d8fd88
Bob Hatamian <bob@purplemass.com>
2019-07-17 17:18:34 +0100

    Close ports when closing application

--- midi_mapper/app.py
+++ midi_mapper/app.py
@@ -51,7 +51,11 @@ if __name__ == "__main__":
 
     def signal_handler(signal, frame) -> None:
         print('\033[H\033[J')
-        print('Keyboard interupt detected')
+        print('Keyboard interupt detected\n')
+        for port in store.get('inports').ports + store.get('outports').ports:
+            if port.closed is False:
+                print(f'Closing {port}')
+                port.close()
         sys.exit(0)
 
     signal.signal(signal.SIGINT, signal_handler)
